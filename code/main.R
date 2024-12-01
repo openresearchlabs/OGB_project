43e972ec12460ca37d7d5c9a471ef580221d8956
antagomir <leo.lahti@iki.fi>
2024-12-01 21:50:50 +0200

    updated main.R

--- code/main.R
+++ code/main.R
@@ -9,34 +9,36 @@
 #  •	diet 1, visit 2	vs.	diet 2, visit 2
 #Was there a difference in microbiota between the diet groups after the treatment?
 
-# setwd("code")
+# Load libraries
 library(quarto)
+
 #load the functions needed
 source("funct.R")
-quarto_bin <- quarto::quarto_path()
-# quarto::quarto_render("Community_composition.qmd")
-# quarto::quarto_render("Alpha_Diversity.qmd")
-# quarto::quarto_render("Beta_Diversity.qmd")
 
-# Render the qmd using quarto cli
-system(paste(shQuote(quarto_bin), "render alpha.qmd --output-dir ../output"))
-system(paste(shQuote(quarto_bin), "render beta.qmd --output-dir ../output"))
-system(paste(shQuote(quarto_bin), "render daa.qmd --output-dir ../output"))
+# Define output directory
+outdir   <- "../output/"
 
-#RUNNING THE FUNCTIONS
-# List of comparisons
+# Define the kist of comparisons
 comparisons <- list(
   c("diet_1_visit_1", "diet_1_visit_2"),
   c("diet_2_visit_1", "diet_2_visit_2"),
   c("diet_1_visit_1", "diet_2_visit_1"),
   c("diet_1_visit_2", "diet_2_visit_2")
 )
+
 # Indices to loop through for alpha diversity plot
-indices <- c("shannon", "observed")
-taxa <- c("genus","species")
-outdir ="./output/"
+indices  <- c("shannon", "observed")
+taxa     <- c("genus","species")
 variable <- "group"
 
-# Render the quarto documents
-quarto::quarto_render("Community_composition_OGB")
-quarto::quarto_render("Alpha_Diversity_OGB")
+# Render the qmd in R
+quarto::quarto_render("alpha.qmd")
+quarto::quarto_render("beta.qmd")
+quarto::quarto_render("daa.qmd")
+
+# Option2: render the qmd using quarto cli
+# quarto_bin <- quarto::quarto_path() # Set path
+#system(paste(shQuote(quarto_bin), "render alpha.qmd --output-dir ../output"))
+#system(paste(shQuote(quarto_bin), "render beta.qmd --output-dir ../output"))
+#system(paste(shQuote(quarto_bin), "render daa.qmd --output-dir ../output"))
+
