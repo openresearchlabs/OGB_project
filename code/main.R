0a1328a366df6961fbf525878791a4a68ba3f91f
antagomir <leo.lahti@iki.fi>
2024-12-01 23:24:31 +0200

    OK

--- code/main.R
+++ code/main.R
@@ -1,29 +1,14 @@
-#the code to run all analysis
-
-# COMPARISONS (PRIMARY)
-#•	diet 1, visit 1	vs. 	diet 1, visit 2
-#•	diet 2, visit 1	vs. 	diet 2, visit 2
-#Did the diet change the microbiota?
-#  •	diet 1, visit 1	vs.	diet 2, visit 1
-#Was the microbiota equal between the diet groups before treatment?
-#  •	diet 1, visit 2	vs.	diet 2, visit 2
-#Was there a difference in microbiota between the diet groups after the treatment?
-
 # Load libraries
 library(quarto)
 
 # Create the data
 # OK to run just once
-source("data.R")
+# source("data.R") # Creates tse.rds
 
-# Render the qmd in R
-quarto::quarto_render("alpha.qmd")
+# Alpha diversity analysis
+quarto::quarto_render("alpha.qmd") # ../output/alpha.html
+
+# TODO
 quarto::quarto_render("beta.qmd")
 quarto::quarto_render("daa.qmd")
-
-# Option2: render the qmd using quarto cli
-# quarto_bin <- quarto::quarto_path() # Set path
-#system(paste(shQuote(quarto_bin), "render alpha.qmd --output-dir ../output"))
-#system(paste(shQuote(quarto_bin), "render beta.qmd --output-dir ../output"))
-#system(paste(shQuote(quarto_bin), "render daa.qmd --output-dir ../output"))
 
